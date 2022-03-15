f606cfd05829a9bfb1bede34644bbdd5a00967e6
Emmanuel Camard <emmanuel.camard@free.fr>
2022-03-15 20:42:26 +0100

    Ajout de la configuration pour la documentation de l'API avec Swagger2

--- src/main/java/com/openclassrooms/mediscreenPatientAssessment/MediscreenPatientAssessmentApplication.java
+++ src/main/java/com/openclassrooms/mediscreenPatientAssessment/MediscreenPatientAssessmentApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableFeignClients("com.openclassrooms.mediscreenPatientAssessment")
+@EnableSwagger2
 public class MediscreenPatientAssessmentApplication {
 
 	public static void main(String[] args) {
